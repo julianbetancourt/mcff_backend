71f8c4888de0522fada3dfd72981d089df80f1ca
Julian Betancourt <julianbetancourt10@gmail.com>
2022-02-11 22:30:14 +0100

    gin hello world

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
+
 
 func main() {
-	fmt.Println("Hello, world. I love Go")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 
 }
